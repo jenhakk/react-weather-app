f910aee39242c8d9f6aa1912713cd509b2421b59
jenhakk <75015030+jenhakk@users.noreply.github.com>
2022-08-01 11:30:37 +0300

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
 const apikey = "aY75OrA8yEsAlVv2Ozffqpf8yoT2SAAY";
-const citysearch = "http://dataservice.accuweather.com/locations/v1/";
-const current = "http://dataservice.accuweather.com/currentconditions/v1/";
+const citysearch = "https://dataservice.accuweather.com/locations/v1/";
+const current = "https://dataservice.accuweather.com/currentconditions/v1/";
 
 
 export { apikey, citysearch, current };
